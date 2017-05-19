12c755be3fb35d462cbe1f1d0c4f508bbbeaab76
Yeehom Foo <48705701@qq.com>
2017-05-20 03:16:10 +0800

    created  spring boot project

---
+++ src/main/java/com/yeehom/ecommerseminiprog/ECommerseMiniprogApplication.java
@@ -0,0 +1,12 @@
+package com.yeehom.ecommerseminiprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerseMiniprogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerseMiniprogApplication.class, args);
+	}
+}
